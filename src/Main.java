56293601c587cadbee1632b490ed68b43717853b
IlyuAfanasev <gecktor.86@mail.ru>
2023-04-28 07:52:53 +0400

    correction

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,14 @@ public class Main {
         Car[] cars = {new Car("Car1", 4), new Car("Car2", 4)};
 
 
-        Truck[] trucks = {new Truck("truck1", 6), new Truck("truck2", 8)};
+        Truck[] trucks = {new Truck("truck1", 6),
+                new Truck("truck2", 8)};
 
-        Bicycle[] bicycles = {new Bicycle("bicycle1", 2), new Bicycle("bicycle2", 2)};
-        Transport[] transports = new WheeledTransport[cars.length + trucks.length + bicycles.length];
+        Bicycle[] bicycles = {new Bicycle("bicycle1", 2),
+                new Bicycle("bicycle2", 2)};
+
+
+        Transport[] transports = new Transport[cars.length + trucks.length + bicycles.length];
 
         for (int i = 0; i < cars.length; i++) {
             transports[i] = cars[i];
